a0cb541271551f2ce75a4ad6468bc80b70c68c64
Kamil Kamil <kdbrasi360@gmail.com>
2020-08-26 16:54:04 +0200

    edit structture and add some styling css

--- src/js/App.js
+++ src/js/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
+
 function App() {
   return (
-    <div className="App">
-      <h1>Let's start</h1>
-    </div>
+    <>
+    <h1>lets start</h1>
+    </>
   );
 }
 
