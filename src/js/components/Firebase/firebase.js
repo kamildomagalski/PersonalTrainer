c5310c7d531d9c19908d68d4176d0dd51b8f389f
Kamil Kamil <kdbrasi360@gmail.com>
2020-08-29 13:50:29 +0200

    fixed issues with signUp, now can create new user

--- src/js/components/Firebase/firebase.js
+++ src/js/components/Firebase/firebase.js
@@ -10,14 +10,6 @@ const config = {
   projectId: process.env.REACT_APP_projectId,
   storageBucket: process.env.REACT_APP_storageBucket,
   messagingSenderId: process.env.REACT_APP_messagingSenderId,
-//   apiKey: 'AIzaSyA8J896gSbmlfDPWZcvNbRttBr3BI25yLc',
-//   authDomain: 'personaltrainer-9aa80.firebaseapp.com',
-//   databaseURL: 'https://personaltrainer-9aa80.firebaseio.com',
-// projectId: 'personaltrainer-9aa80',
-// storageBucket :'personaltrainer-9aa80.appspot.com',
-// messagingSenderId :'682054874701',
-// appId : '1:682054874701:web:16c71b7de41c4e8d390e04',
-// measurementId : 'G-JY4H2KY8ZN',
 };
 
 class Firebase {
--- src/js/landing_page/signUp_page.js
+++ src/js/landing_page/signUp_page.js
@@ -1,8 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import CloseX from "../components/closeX";
-import {FirebaseContext} from '../components/Firebase/indexFirebase';
-import {Link} from "react-router-dom";
-import Firebase from '../components/Firebase/indexFirebase';
+import { FirebaseContext } from '../components/Firebase/indexFirebase';
+
 
 function SignUpPage({isSignUpShown, handleSignUpVisible, handleLoginVisible}) {
   
@@ -19,87 +18,95 @@ function SignUpPage({isSignUpShown, handleSignUpVisible, handleLoginVisible}) {
     handleLoginVisible(true)
   }
   
-  useEffect(() => {
-    this.firebase = new Firebase();
-  }, {});
+
+  const SignUpForm = ({firebase}) => {
+    
+    const handleSubmit = (event) => {
+      event.preventDefault()
+      firebase
+        .doCreateUserWithEmailAndPassword(initialState.email, initialState.passwordOne)
+        .then(authUser => {
+          setState({...initialState});
+        })
+        .catch(error => {
+            setState({...initialState, error})
+          }
+        );
+    }
   
-  const handleChange = (event) => {
-    const {name, value} = event.target;
-    setState(prevState => {
-      return {
-        ...prevState,
-        [name]: value
-      }
-    })
-  }
-  
-  const isInvalid =
-    initialState.passwordOne !== initialState.passwordTwo ||
-    initialState.passwordOne === '' ||
-    initialState.email === '' ||
-    initialState.username === '';
-  
-  const handleSubmit = (event) => {
-    event.preventDefault()
-    this.firebase
-      .doCreateUserWithEmailAndPassword(initialState.email, initialState.passwordOne)
-      .then(authUser => {
-        setState({...initialState});
-      })
-      .catch(error => {
-          setState({...initialState, error})
+    const handleChange = (event) => {
+      const {name, value} = event.target;
+      setState(prevState => {
+        return {
+          ...prevState,
+          [name]: value
         }
-      );
+      })
+    }
+  
+    const isInvalid =
+      initialState.passwordOne !== initialState.passwordTwo ||
+      initialState.passwordOne === '' ||
+      initialState.email === '' ||
+      initialState.username === '';
+    
+    return (
+      <>
+        <form onSubmit={handleSubmit}>
+          <input
+            name={'username'}
+            value={initialState.username}
+            type={'text'}
+            className={'signUp__input'}
+            placeholder={'Username'}
+            onChange={handleChange}
+          />
+          <input
+            name={'email'}
+            value={initialState.email}
+            type={'email'}
+            className={'signUp__input'}
+            placeholder={'Email'}
+            onChange={handleChange}
+          />
+          <input
+            name={'passwordOne'}
+            value={initialState.passwordOne}
+            type={'password'}
+            className={'signUp__input'}
+            placeholder={'Password'}
+            onChange={handleChange}
+          />
+          <input
+            name={'passwordTwo'}
+            value={initialState.passwordTwo}
+            type={'password'}
+            className={'signUp__input'}
+            placeholder={'Confirm Password'}
+            onChange={handleChange}
+          />
+          <button className={'btn signUp__btn'} disabled={isInvalid} type={'submit'}>
+            CREATE ACCOUNT
+          </button>
+          {/*{error && <p>{error.message}</p>}*/}
+        </form>
+      </>
+    )
   }
   
   return (
+    
     <section className={isSignUpShown ? 'signUp' : 'signUp hidden'}>
       <div className={'board signUp__board'}>
         <CloseX onClick={handleOff}/>
         <h2 className={'signUp__title'}>Sign Up Now!</h2>
         <FirebaseContext.Consumer>
-          <form onSubmit={handleSubmit}>
-            <input
-              name={'username'}
-              value={initialState.username}
-              type={'text'}
-              className={'signUp__input'}
-              placeholder={'Username'}
-              onChange={handleChange}
-            />
-            <input
-              name={'email'}
-              value={initialState.email}
-              type={'email'}
-              className={'signUp__input'}
-              placeholder={'Email'}
-              onChange={handleChange}
-            />
-            <input
-              name={'passwordOne'}
-              value={initialState.passwordOne}
-              type={'password'}
-              className={'signUp__input'}
-              placeholder={'Password'}
-              onChange={handleChange}
-            />
-            <input
-              name={'passwordTwo'}
-              value={initialState.passwordTwo}
-              type={'password'}
-              className={'signUp__input'}
-              placeholder={'Confirm Password'}
-              onChange={handleChange}
-            />
-            <button className={'btn signUp__btn'} disabled={isInvalid} type={'submit'}>
-              CREATE ACCOUNT
-            </button>
-            {/*{error && <p>{error.message}</p>}*/}
-          </form>
+          {firebase => <SignUpForm firebase={firebase}/>}
         </FirebaseContext.Consumer>
       </div>
     </section>
-  );
+  
+  )
 }
 
 export default SignUpPage;
