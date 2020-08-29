1fabb815ad0d47d101fa627dd727a0e01a7998c4
Kamil Kamil <kdbrasi360@gmail.com>
2020-08-29 07:58:04 +0200

    add history and exercises app components

---
+++ src/js/application/exercises.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Exercises() {
+ 
+ return (
+   <section className={'exercises'}>
+     <h2 className={'calendar__title'}>Exercises</h2>
+   </section>
+ );
+}
+export default Exercises;
