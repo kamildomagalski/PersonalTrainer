654df7f7a1511c1e1bcffd8edbb1e6031a98343e
Kamil Kamil <kdbrasi360@gmail.com>
2020-09-02 13:20:27 +0200

    Add buttons on tabs in AllExercises Component, code refactor

--- src/js/application/AllExercises.js
+++ src/js/application/AllExercises.js
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import AddNewExercise from "./AddNewExercise";
 import ShowExerciseInfo from "./ShowExerciseInfo";
 import AddExercise from "../components/AddExercise";
 
 
-function AllExercises({exercises,isAllExercisesVisible }) {
+function AllExercises({exercises, isAllExercisesVisible}) {
   const [exerciseInfo, setExerciseInfo] = useState([])
   const [isAddNewExVisible, setAddNewExVisible] = useState(false)
   const [isExInfoVisible, setExInfoVisible] = useState(false)
@@ -27,49 +27,57 @@ function AllExercises({exercises,isAllExercisesVisible }) {
   }
   
   return (
-   <div className={isAllExercisesVisible ? 'allExercises__search': 'allExercises__search hidden' }>
-     <div className={'allExercises__filter'}>
-       <form className={'allExercises__form'}>
-         <select name={'difficulty'} placeholder={'difficulty'}>
-           <option value={'easy'}>Easy</option>
-           <option value={'medium'}>Medium</option>
-           <option value={'hard'}>Hard</option>
-         </select>
-         <select name={'muscle group'} placeholder={'muscle group'}>
-           <option value={'upper body'}>Upper body</option>
-           <option value={'lower body'}>Lower body</option>
-         </select>
-         <select name={'type'} placeholder={'type'}>
-           <option value={'indoor'}>Indoor</option>
-           <option value={'outdoor'}>Outdoor</option>
-         </select>
-       </form>
-     </div>
-     <div className={'allExercises__display'}>
-       <div onClick={handleOn} className={'allExercises__box allExercises__box-add'}> Add exercise</div>
-       {exercises.map(exercise => {
-         return (
-           <div className={'allExercises__box'}
-                onClick={(event) => {
-                  handleInfoOn();
-                  handleEXInfo(exercise);
-                }} key={exercise.id}>
-             <AddExercise/>
-             {exercise.name}</div>
-         )
-       })}
-     </div>
-    
-     <AddNewExercise
-       isAddNewExVisible={isAddNewExVisible}
-       handleAddNewExVisible={handleAddNewExVisible}/>
-    
-     <ShowExerciseInfo
-       isExInfoVisible={isExInfoVisible}
-       handleExInfoVisible={handleExInfoVisible}
-       clearExerciseInfo={clearExerciseInfo}
-       exerciseInfo={exerciseInfo}/>
-   </div>
- );
+    <div className={isAllExercisesVisible ? 'allExercises__search' : 'allExercises__search hidden'}>
+      <div className={'allExercises__filter'}>
+        <form className={'allExercises__form'}>
+          <select name={'difficulty'} placeholder={'difficulty'}>
+            <option value={'easy'}>Easy</option>
+            <option value={'medium'}>Medium</option>
+            <option value={'hard'}>Hard</option>
+          </select>
+          <select name={'muscle group'} placeholder={'muscle group'}>
+            <option value={'upper body'}>Upper body</option>
+            <option value={'lower body'}>Lower body</option>
+          </select>
+          <select name={'type'} placeholder={'type'}>
+            <option value={'indoor'}>Indoor</option>
+            <option value={'outdoor'}>Outdoor</option>
+          </select>
+        </form>
+      </div>
+      <div className={'allExercises__display'}>
+        <div className={'allExercises__box allExercises__box-add'}>
+          <button onClick={handleOn}>
+            Add exercise
+          </button>
+        </div>
+        {exercises.map(exercise => {
+          return (
+            <div className={'allExercises__box'}
+                 key={exercise.id}>
+              <button onClick={(event) => {
+                handleInfoOn();
+                handleEXInfo(exercise);
+              }}>Show info
+              </button>
+              <AddExercise/>
+              <p className={'boxTitle'}>{exercise.name}</p>
+            </div>
+          )
+        })}
+      </div>
+      
+      <AddNewExercise
+        isAddNewExVisible={isAddNewExVisible}
+        handleAddNewExVisible={handleAddNewExVisible}/>
+      
+      <ShowExerciseInfo
+        isExInfoVisible={isExInfoVisible}
+        handleExInfoVisible={handleExInfoVisible}
+        clearExerciseInfo={clearExerciseInfo}
+        exerciseInfo={exerciseInfo}/>
+    </div>
+  );
 }
+
 export default AllExercises;
