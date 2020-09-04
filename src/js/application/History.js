cd8955847a127ab58cf611417a97ec5d521d8227
Kamil Kamil <kdbrasi360@gmail.com>
2020-09-04 17:18:47 +0200

    add content do 'under development pages'

--- src/js/application/History.js
+++ src/js/application/History.js
@@ -4,6 +4,9 @@ function History() {
  return (
    <section className={'calendar'}>
      <h2 className={'calendar__title'}>History</h2>
+     <div className={'underConstruction'}>
+       <h2 className={'underConstruction__title'}>Page on development stage</h2>
+     </div>
    </section>
  );
 }
