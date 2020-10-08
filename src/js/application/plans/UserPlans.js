c4ebce5fef644c2482273a39bbc555161f42b233
Kamil Domagalski <kdbrasi360@gmail.com>
2020-10-08 11:30:52 +0200

    add create plan button

--- src/js/application/plans/UserPlans.js
+++ src/js/application/plans/UserPlans.js
@@ -9,7 +9,7 @@ function UserPlans() {
  return (
   <div className={'userPlans'}>
    <div className={'userPlans__display'}>
-    <div className={'userPlans__box allPlans__box-add'}>
+    <div className={'userPlans__box userPlans__box-add'}>
      <button className={'btn userPlans__btn'} >
       Create plan
      </button>
